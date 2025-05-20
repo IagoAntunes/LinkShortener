751218e7b87cce07df50d808f8ee34e92e37fac3
IagoAntunes <iagoantunes.f@gmail.com>
2025-05-19 23:10:27 -0300

    feat: Creating colors, typography themes and creating main layout

---
+++ link-shortener-app/app/src/main/java/com/iagoaf/linkshortener/LinkItemFragment.kt
@@ -0,0 +1,29 @@
+package com.iagoaf.linkshortener
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.iagoaf.linkshortener.placeholder.PlaceholderContent
+
+
+class LinkItemFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.link_fragment_item_list, container, false)
+
+        return view
+    }
+
+}
